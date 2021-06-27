015d0e65e061b3ae1578300939d649258440297a
Ben Gottlieb <ben@standalone.com>
2021-06-27 15:10:12 -0500

    Initial Commit

---
+++ NavigationContainer/NavigationContainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationContainerApp.swift
+//  NavigationContainer
+//
+//  Created by Ben Gottlieb on 6/27/21.
+//
+
+import SwiftUI
+
+@main
+struct NavigationContainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
